27c86d66cc303711ce4aa8f08006d13ea1d07bc6
fabianluescher <info@fluescher.ch>
2016-09-16 17:17:22 +0200

    Yapf'd

--- waldo.py
+++ waldo.py
@@ -9,7 +9,6 @@ import os
 import sys
 from waldo_fn import *
 import click
-
 '''
 # from os import system
 import serial
@@ -25,7 +24,6 @@ from waldo_fn import *
 # path to main dir
 mainpath = os.path.dirname(os.path.realpath(__file__))
 
-
 # ===========================================================================
 # MAIN
 # ===========================================================================
@@ -39,31 +37,30 @@ if __name__ == '__main__':
         for arguments in sys.argv:
             arg.append(arguments)
 
-
         if arg[1] == "-h" or arg[1] == "--help":
             # HELP
             helpfile(mainpath)
-            
+
         elif arg[1] == "-c" or arg[1] == "--connection":
             # SET CONNECTION FOR RECORDING
             setconnection(mainpath, arg[2])
-            
-        elif arg[1]== "-r" or arg[1] == "--record":
+
+        elif arg[1] == "-r" or arg[1] == "--record":
             # RECORD
             record_setup(mainpath, arg)
-            
+
         elif arg[1] == "-sp" or arg[1] == "--singleplay":
             # SINGLE PLAY
             singleplay(mainpath, arg)
-            
+
         elif arg[1] == "-p" or arg[1] == "--play":
             # PLAY ALL
             playall(mainpath, arg)
-            
+
         elif arg[1] == "-n" or arg[1] == "--new":
             # CREATE NEW FOLDER SYSTEM
             newproject(mainpath, arg)
-            
+
         elif arg[1] == "-ls" or arg[1] == "--list":
             # LIST PROJECTS AND CHANNELS
             listprojects(mainpath)
@@ -75,8 +72,7 @@ if __name__ == '__main__':
         else:
             # SAY WHAT?
             print "How did you do that? Type 'waldo.py -h' for help."
-            
-    
+
     except KeyboardInterrupt:
         print "\nExit by user."
         # Set GPIO to default
