118c82c1ffeef48cf63c727da7a476734774914c
fabianluescher <info@fluescher.ch>
2018-01-17 15:01:33 +0100

    Configured shutdown button waldo specific

--- shutdownbutton.py
+++ shutdownbutton.py
@@ -1,4 +1,3 @@
-
 import RPi.GPIO as GPIO
 import time, datetime, os
 
@@ -19,20 +18,21 @@ numberOfMeasurements = 5
 
 # Button pin setup
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(14, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+button_pin = 19
+GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # Power Led pin setup
-GPIO.setup(15, GPIO.OUT)
-GPIO.output(15, True)
+led_primary_pin = 16
+GPIO.setup(led_primary_pin, GPIO.OUT)
+GPIO.output(led_primary_pin, True)
 
 signalValues = [True for i in range(numberOfMeasurements)]
 
 while True:
-    input_state = GPIO.input(14)
+    input_state = GPIO.input(button_pin)
     signalValues.pop(0)
     signalValues.append(input_state)
     if signalValues.count(False) >= 3:
         print('Shutdown button pressed')
         os.system("shutdown now -h")
     time.sleep(waitingTime)
-
