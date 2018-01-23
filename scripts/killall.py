f177f1dc32d42f5cd32594aa148348c2118eca21
fabianluescher <info@fluescher.ch>
2018-01-23 16:10:55 +0100

    Added killall.py script

---
+++ scripts/killall.py
@@ -0,0 +1,4 @@
+# TODO: killall autostarted scripts
+# python /home/pi/Scripts/waldo/waldo.py -ap 2 &
+# python /home/pi/Scripts/waldo/shutdown_button.py &
+# python /home/pi/Scripts/waldo/numpad_listener.py &
