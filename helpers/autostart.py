78baae9c47626211e0963b86e7d0a8b92f7945f6
fabianluescher <info@fluescher.ch>
2018-01-24 12:07:34 +0100

    Moved helpers folder

---
+++ helpers/autostart.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import sys
+from os import path
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+from waldo.utils import read_config
+
+# Read preferences
+PREFERENCES = read_config(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
+autostart = PREFERENCES['autostart']
+
+if autostart:
+    # modifier = '-ap %s' % autostart if int(autostart) else ''
+    if autostart is not True:
+        modifier = '-ap %s' % autostart
+    else:
+        modifier = ''
+    # os.system('python /home/pi/Scripts/waldo/waldo/helpers/remote.py %s' % modifier)
+    # os.system('python /home/pi/Scripts/waldo/waldo/helpers/shutdown_button.py')
+    # os.system('python /home/pi/Scripts/waldo/waldo/helpers/numpad_listener.py')
