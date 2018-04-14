d218e7c0d9c94e37748dc19b7454badd41b0fad9
fabianluescher <info@fluescher.ch>
2018-04-14 14:55:40 +0200

    Add temperature monitoring and do so with autostart

---
+++ helpers/monitor_temparature.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import os
+import time
+import datetime
+
+
+def measure_temp():
+    temp = os.popen("vcgencmd measure_temp").readline()
+    temp = temp.replace("temp=", "")
+    temp = float(temp.replace("'C", ""))
+    time = datetime.datetime.now().strftime('%a, %Y-%m-%d %H:%M:%S')
+    visual = int(temp)/10 * "█"
+
+    return time, "\t", str(temp) + '°C', "\t", visual
+
+
+while True:
+    data = ""
+    for m in measure_temp():
+         data += str(m)
+
+    print data
+    data += "\n"
+    file = open("helpers/temperature_log", "a")
+    file.write(data)
+    file.close()
+    time.sleep(60)
