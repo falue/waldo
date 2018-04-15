35db9ab61ef0ebf79c5681e39dddb555b80ca9aa
fabianluescher <info@fluescher.ch>
2018-04-15 13:23:02 +0200

    Autostart changes REC_REPL, shorter paths

--- helpers/autostart.py
+++ helpers/autostart.py
@@ -10,10 +10,18 @@ import subprocess as sp
 from os import path
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 from waldo.utils import read_config
+from waldo.fn import change_glob_rec_repl
 
 # Read preferences
 PREFERENCES = read_config(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
 autostart = PREFERENCES['autostart']
+
+main_path = os.path.expanduser('~/Scripts/waldo')
+
+# reset config file value recrepl (in case waldo is shut off unexpected):
+change_glob_rec_repl(False)
+
+# os.system('%s/helpers/crash_recover.sh' % main_path)  # commented because it shuts down because autostart is running "now"
 
 if autostart:
     # if autostart is number:
@@ -21,8 +29,9 @@ if autostart:
         modifier = autostart
     else:
         modifier = ''
-    main_path = os.path.expanduser('~/Scripts/waldo')
-    sp.Popen(['python', '/home/pi/Scripts/waldo/helpers/shutdown_button.py'])
-    os.system('sudo python /home/pi/Scripts/waldo/helpers/monitor_temperature.py')
-    os.system('sudo python /home/pi/Scripts/waldo/helpers/remote.py -ap %s' % modifier)
-    os.system('python %s/helpers/numpad_listener.py' % main_path)
+
+    sp.Popen(['python', '%s/helpers/shutdown_button.py' % main_path])
+    # os.system('sudo python %s/helpers/monitor_temperature.py' % main_path)
+    sp.Popen(['python', '%s/helpers/monitor_temperature.py' % main_path])
+    os.system('sudo python %s/helpers/remote.py -ap %s' % (main_path, modifier))
+    # os.system('python %s/helpers/numpad_listener.py' % main_path)
