382c2a6da4afc891615deed7778b1dbdb2e6ef1d
fabianluescher <info@fluescher.ch>
2018-08-03 16:32:38 +0200

    Set temperature logs in config file

--- helpers/autostart.py
+++ helpers/autostart.py
@@ -15,6 +15,7 @@ from waldo.fn import change_glob_rec_repl
 # Read preferences
 PREFERENCES = read_config(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
 autostart = PREFERENCES['autostart']
+measure_temp = PREFERENCES['measure_temp']
 
 main_path = os.path.expanduser('~/Scripts/waldo')
 
@@ -23,10 +24,14 @@ change_glob_rec_repl(False)
 
 # os.system('%s/helpers/crash_recover.sh' % main_path)  # commented because it shuts down because autostart is running "now"
 
+if measure_temp:
+    # os.system('sudo python %s/helpers/monitor_temperature.py' % main_path)
+    sp.Popen(['python', '%s/helpers/monitor_temperature.py' % main_path])
+
 if autostart:
     # Notify about autostart
-    bashcommando = 'play %s/waldo/sounds/chime.mp3 -q' % main_path
-    os.system(bashcommando)  # invoke 'sox' in quiet mode
+    # bashcommando = 'play %s/waldo/sounds/chime.mp3 -q' % main_path
+    # os.system(bashcommando)  # invoke 'sox' in quiet mode
 
     # if autostart is number:
     if autostart is not True:
